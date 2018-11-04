567a04faa8d5683ea3665efd490bf54b322cf2cb
User <User@WIN-R1VEPF5FUNI>
2018-11-04 17:28:03 +0300

    task_DEV_5

---
+++ DEV_5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DEV_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function carname = new Function();
+            Car carf = new Car();
+            carname.carf();
+        }
+    }
+}
