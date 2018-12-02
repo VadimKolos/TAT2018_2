1135c247a443f95be312d765a415c951f5329b74
User <User@WIN-R1VEPF5FUNI>
2018-12-02 17:49:55 +0300

    DEV_8

---
+++ DEV8/CarMaster.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DEV8
+{
+    class CarMaster
+    {
+        public void master()
+        {
+
+            Car carname = new Car();
+            Function functionName = new Function();
+            functionName.carf();
+        }
+    }
+}
