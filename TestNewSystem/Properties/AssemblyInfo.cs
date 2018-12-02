8f6f48e78da6b781354ad4f1ae155ab7796c3f4e
User <User@WIN-R1VEPF5FUNI>
2018-12-02 15:15:05 +0300

    DEV 7

---
+++ TestNewSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNewSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNewSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4563dd9-718c-4c01-947e-b7a0e6b4ea4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNewSystem/UnitTestDEV3.cs
@@ -0,0 +1,23 @@
+﻿using DEV_7;
+using NUnit.Framework;
+
+
+namespace TestNewSystem
+{
+    [TestFixture]
+    public class NUnitTestNewSystem
+    {
+        NewSystem test = new NewSystem();
+
+        [TestCase("7", 2, "111")]
+        [TestCase("0", 15, "0")]
+        [Test]
+        
+        public void input_7and2_111returned(string inputValue, int countSystem, string result)
+        {
+            string tests = test.Convert_count_System(inputValue, countSystem);
+            Assert.AreEqual(tests, result);
+        }
+       
+    }
+}
